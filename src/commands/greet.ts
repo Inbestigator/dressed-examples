a713788d662e4ad606be992c5f46078e38e52ae5
Inbestigator <inbestigatoryt@gmail.com>
2024-11-15 21:25:50 -0800

    Simplified greet

--- src/commands/greet.ts
+++ src/commands/greet.ts
@@ -8,16 +8,8 @@ export const config: CommandConfig = {
 };
 
 export default async function greet(interaction: CommandInteraction) {
-  if ("member" in interaction && interaction.member) {
-    await interaction.reply({
-      content:
-        `Hey there, ${interaction.member.user.global_name}! Cool server!`,
-      ephemeral: true,
-    });
-  } else if ("user" in interaction && interaction.user) {
-    await interaction.reply({
-      content: `Hey there, ${interaction.user.global_name}! Nice DMs!`,
-      ephemeral: true,
-    });
-  }
+  await interaction.reply({
+    content: `Hey there, ${interaction.user.global_name}! Cool server!`,
+    ephemeral: true,
+  });
 }
