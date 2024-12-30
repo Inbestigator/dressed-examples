17eace707591bc95a310a90f19ce4b409599d84c
Inbestigator <inbestigatoryt@gmail.com>
2024-12-30 03:12:15 -0800

    Bumped versions

--- deno/simple/bot.config.ts
+++
@@ -1,8 +0,0 @@
-import type { BotConfig } from "@dressed/dressed";
-
-const config: BotConfig = {
-  clientId: "", // Recommended to replace with your bot's client ID
-  deno: true,
-};
-
-export default config;
