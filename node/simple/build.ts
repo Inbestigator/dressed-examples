798ee4754a51025071bdc7bb7d2365d6ff5f552c
Inbestigator <inbestigatoryt@gmail.com>
2025-01-11 14:27:49 -0800

    No need for custom server anymore

--- node/simple/build.ts
+++ node/simple/build.ts
@@ -2,17 +2,10 @@ import { build } from "@dressed/dressed/server";
 import { writeFileSync } from "node:fs";
 
 async function genBot() {
-  const outputContent = await build(true, false);
-  const nodeServer = outputContent
-    .replace(
-      /server";\n/,
-      '$&import { createServer } from "./server.ts";\n',
-    )
-    .replace(
-      /console.warn\(.+\)/,
-      "createServer(runCommand, runComponent, config)",
-    );
-  writeFileSync("./bot.gen.ts", new TextEncoder().encode(nodeServer));
+  writeFileSync(
+    "./bot.gen.ts",
+    new TextEncoder().encode(await build(true)),
+  );
   console.log("✔ Wrote to bot.gen.ts");
 }
 
